1df03619a8986cab40396934011c03cf94d6df6d
conrado julio <conradojuliosisnero@gmail.com>
2023-08-30 15:12:08 -0500

    ultimos pasos de codigo

--- index.php
+++ index.php
@@ -4,8 +4,8 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/tareas/app-tareas/includes/assets/css/style.css">
-    <link rel="stylesheet" href="http://localhost/tareas/app-tareas/includes/assets/css/task-completed.css">
+    <link rel="stylesheet" href="http://localhost/tareas/app-tareas/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/tareas/app-tareas/assets/css/task-completed.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Open+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
--- task.php
+++ task.php
@@ -4,7 +4,7 @@
 <head>
     <meta charset="UTF-8">
     <meta name="viewport" content="width=device-width, initial-scale=1.0">
-    <link rel="stylesheet" href="http://localhost/tareas/app-tareas/includes/assets/css/style.css">
+    <link rel="stylesheet" href="http://localhost/tareas/app-tareas/assets/css/style.css">
     <link rel="preconnect" href="https://fonts.googleapis.com">
     <link rel="preconnect" href="https://fonts.gstatic.com" crossorigin>
     <link href="https://fonts.googleapis.com/css2?family=Dancing+Script:wght@400;500;600;700&family=Open+Sans:wght@300;400;500;600&display=swap" rel="stylesheet">
@@ -16,28 +16,30 @@
 
         <!-- formulario para gregar tareas  -->
         <form method="post" class="form">
-            <div><h1 class="tittle">APP TASK</h1></div>
+            <div>
+                <h1 class="tittle">APP TASK</h1>
+            </div>
             <div class="form__tittle"><label>Añada aqui, sus tareas de manera facil</label></div>
             <textarea class="add__task" name="tarea" cols="30" rows="10"></textarea>
             <div class="contend__btn">
-            <button class="btn-53" name="insert" type="submit">
-                <div class="original">Añadir Tarea</div>
-                <div class="letters">
-                    <span>A</span>
-                    <span>ñ</span>
-                    <span>a</span>
-                    <span>d</span>
-                    <span>i</span>
-                    <span>r</span>
-                </div>
-            </button>
+                <button class="btn-53" name="insert" type="submit">
+                    <div class="original">Añadir Tarea</div>
+                    <div class="letters">
+                        <span>A</span>
+                        <span>ñ</span>
+                        <span>a</span>
+                        <span>d</span>
+                        <span>i</span>
+                        <span>r</span>
+                    </div>
+                </button>
 
-            <button class="btn-53">
-                <div class="original">Ver Tareas</div>
-                <div class="letters" href="index.php">
-                    <span class="letters__link"><a class="link" href="index.php">Ver</a></span>
-                </div>
-            </button>
+                <button class="btn-53">
+                    <div class="original">Ver Tareas</div>
+                    <div class="letters" href="index.php">
+                        <span class="letters__link"><a class="link" href="index.php">Ver</a></span>
+                    </div>
+                </button>
             </div>
         </form>
         <!-- validacion en caso que el campo este vacio  -->
@@ -57,18 +59,22 @@
                 // guardamos la informacion del textarea en la BD 
                 $query = $conect->query("INSERT INTO task (tarea) VALUES ('$tarea')");
 
-                // si query fue exitoso redirigimos a tareas completadas 
+                // si query fue exitoso Mostramos esto...
                 if ($query) {
-                    header("location: index.php");
+                    ?>
+                    <div class="label__sucessfull">
+                        <label>Agregado con exito!</label>
+                    </div>
+                <?php
                 }
             } else {
 
-                // imprimimos esto en pantalla 
-        ?>
+                // Si no imprimimos esto en pantalla 
+                ?>
                 <div class="label__error">
                     <label>Error el campo esta vacio</label>
                 </div>
-        <?php
+                <?php
             }
         }
 
